4b8bccbf4436b8d54e6b7c528b59adcf46eb6edc
Pedro <pedro.remedios@gmail.com>
2018-04-20 13:18:44 +0100

    changes

--- data.js
+++ data.js
@@ -69,7 +69,7 @@ function renderCharts() {
                 console.log(results[1]);
                 var data1 = results[0].rows.filter(function(row) { return row[2] !== "0"; }).map(function(row) { return +row[2]; });
                 var data2 = results[1].rows.filter(function(row) { return row[2] !== "0"; }).map(function(row) { return +row[2]; });
-                var labels = results[1].rows.filter(function(row) { return row[2] !== "0"; }).map(function(row) { return +row[0]; });
+                var labels = results[0].rows.filter(function(row) { return row[2] !== "0"; }).map(function(row) { return +row[0]; });
 
                 labels = labels.map(function(label) {
                     return moment(label, 'YYYYMMDD').format('ddd');
