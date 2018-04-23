69d5846ff9e2a7b4ebcbde905b5c1ee5f3a30857
Pedro <pedro.remedios@gmail.com>
2018-04-23 11:30:06 +0100

    changes

--- data.js
+++ data.js
@@ -256,7 +256,7 @@ function renderCharts() {
         function accountPermission(accountId) {
             return getWebProperties(accountId).then(items => items.forEach(item => {
                 getProfiles(item.accountId, item.id).then(items => items.forEach(item => {
-                    getProfileData(item.id).then(console.log(response)).then(console.log(response));
+                    getProfileData(item.id).then(response => console.log(response)).then(response => console.log(response));
                 }));
             }));
         }
