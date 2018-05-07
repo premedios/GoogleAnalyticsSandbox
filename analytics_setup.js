d08ac07381b28599cd8fe9467f7ed05b033c9371
Pedro <pedro.remedios@gmail.com>
2018-05-07 17:58:29 +0100

    'changes'

--- analytics_setup.js
+++ analytics_setup.js
@@ -14,22 +14,3 @@
         g.load('analytics')
     };
 }(window, document, 'script'));
-
-window.onGoogleYoloLoad = (googleyolo) => {
-    console.log("Loaded");
-    const retrievePromise = googleyolo.retrieve({
-        supportedAuthMethods: [
-            "https://accounts.google.com",
-            "googleyolo://id-and-password"
-        ],
-        supportedIdTokenProviders: [{
-            uri: "https://accounts.google.com",
-            clientId: "708383383102-4h03gssp03i8ceonmqm14a44eugq9dh5.apps.googleusercontent.com"
-        }]
-    }).then((credential) => {
-        console.log("Retreive: " + credential);
-    }, (error) => {
-        console.log(error);
-    });
-
-};
