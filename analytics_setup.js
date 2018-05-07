d6a768e6ba9b18bad802239491d4e0785d4e7a06
Pedro <pedro.remedios@gmail.com>
2018-05-07 17:53:06 +0100

    changes

--- analytics_setup.js
+++ analytics_setup.js
@@ -1,16 +1,35 @@
 (function(w, d, s, g, js, fjs) {
-  g = w.gapi || (w.gapi = {});
-  g.analytics = {
-      q: [],
-      ready: function(cb) {
-          this.q.push(cb)
-      }
-  };
-  js = d.createElement(s);
-  fjs = d.getElementsByTagName(s)[0];
-  js.src = 'https://apis.google.com/js/platform.js';
-  fjs.parentNode.insertBefore(js, fjs);
-  js.onload = function() {
-      g.load('analytics')
-  };
+    g = w.gapi || (w.gapi = {});
+    g.analytics = {
+        q: [],
+        ready: function(cb) {
+            this.q.push(cb)
+        }
+    };
+    js = d.createElement(s);
+    fjs = d.getElementsByTagName(s)[0];
+    js.src = 'https://apis.google.com/js/platform.js';
+    fjs.parentNode.insertBefore(js, fjs);
+    js.onload = function() {
+        g.load('analytics')
+    };
 }(window, document, 'script'));
+
+window.onGoogleYoloLoad = (googleyolo) => {
+    console.log("Loaded");
+    const retrievePromise = googleyolo.retrieve({
+        supportedAuthMethods: [
+            "https://accounts.google.com",
+            "googleyolo://id-and-password"
+        ],
+        supportedIdTokenProviders: [{
+            uri: "https://analytics.google.com",
+            clientId: "708383383102-4h03gssp03i8ceonmqm14a44eugq9dh5.apps.googleusercontent.com"
+        }]
+    }).then((credential) => {
+        console.log("Retreive: " + credential);
+    }, (error) => {
+        console.log(error);
+    });
+
+};
